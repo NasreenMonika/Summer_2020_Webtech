037a28134498a9d748a93b31cfbfde41140f560f
NasreenMonika <62530607+NasreenMonika@users.noreply.github.com>
2020-07-29 17:20:07 +0600

    registration

--- Cookie_Task/checkRegistration.php
+++ Cookie_Task/checkRegistration.php
@@ -1,34 +1,40 @@
 <?php
-	session_start();
 
-	if(isset($_POST['submit'])){
+	if(isset($_POST['submit']))
+	{
 
 		$name = $_POST['name'];
-		$uname = $_POST['username'];
+		$uname = $_POST['userName'];
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 		$confirmPassword = $_POST['confirmPassword'];
 		$gender = $_POST['gender'];
 		$day = $_POST['day'];
-		$month
+		$month = $_POST['month'];
+		$year = $_POST['year'];
 
-		if(empty($uname) || empty($password) || empty($email)){
+		if(empty($uname) || empty($password) || empty($email)|| empty($name)|| empty($confirmPassword)||empty($day)||empty($month)||empty($year)||empty($gender))
+		{
 			echo "null submission";
-		}else {
-
-			$user = [
-						'uname'=>$uname,
-						'email'=>$email,
-						'password'=>$password
-					];
-
-			$_SESSION['uname'] 		= $uname;
-			$_SESSION['email'] 		= $email;
-			$_SESSION['password'] 	= $password;
-			$_SESSION['user'] 		= $user;
-
-
-			header('location: login.html');
+		}
+		else
+		{
+			if($confirmPassword==$password)
+			{
+				setcookie('username', $uname, time()+3600, '/');
+				setcookie('name', $name, time()+3600, '/');
+				setcookie('password', $password, time()+3600, '/');
+				setcookie('email', $email, time()+3600, '/');
+				setcookie('gender', $gender, time()+3600, '/');
+				setcookie('day', $day, time()+3600, '/');
+				setcookie('month',$month, time()+3600,'/');
+				setcookie('year',$year, time()+3600,'/');
+				header('location: login.html');
+			}
+			else
+			{
+				echo "Match your password";
+			}
 		}
 
 	}else{
