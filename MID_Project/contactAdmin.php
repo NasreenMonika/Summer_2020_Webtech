ded4533a9721eb5c2b95481d085d79258635d426
NasreenMonika <62530607+NasreenMonika@users.noreply.github.com>
2020-08-13 08:39:20 +0600

    contact admin

---
+++ MID_Project/contactAdmin.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+	<table  width="700px">
+			<tr>
+				<td height="20px" width="100px">
+					<b><p style="font-size:20px">Admin</p></b>
+				</td>
+			</tr>
+			<tr>
+				<td height="20px" width="30px">
+				<p style="font-size:20px">you are doing a great job</p></b>
+				</td>
+			</tr>
+	</table>
+	<center><br><br><br><br>
+		<table  width="700px">
+			<tr>
+				<td height="20px" width="100px">
+					<input name="sendAdmin" type="text">
+				</td>
+				<td>
+				<input name="submit" type="submit" value="SEND">
+			</td>
+			</tr>
+		
+	</center>
+	
+	</table>
+
+</body>
+</html>
